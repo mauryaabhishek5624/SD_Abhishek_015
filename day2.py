7b64313f1e8ee3e2e2e45a56eba5b6d788e5b035
Abhishek Maurya <mauryaabhishek5624@gmail.com>
2026-07-03 12:21:13 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,104 @@
+# keys = ["name","age"]
+# values = ["abhishek",20]
+# mydict = dict((keys,values))
+# print(mydict)
+
+# l=[1 , 23 , 45 , "abhishek" ]
+# for i in range(len(l)):
+#      print([i],type(l[i]))
+
+# l =["abhishek","alok","ankit"]
+# for i in l:
+#   if i == "alok":
+#     continue
+#   print(i)
+
+# l =["abhishek","alok","ankit"]
+# for i in l:
+#   if i == "ankit":
+#     break
+#   print(i)
+
+# print(list(range(0,5,1)))
+# print(list(range(0,20,2)))
+# print(list(range(-10,0,2)))
+# print(list(range(-8,0,4)))
+
+# l1 = ['sudh','kumar','krish','naik']
+# print(len(l1))
+# print(list(range(len(l1))))
+# for i in range(len(l1)):
+    # print(l1[i])
+# for i in range(len(l1)-1,-1,-1):
+#     print(l1[i])
+
+# for i in (3,2,1,0):
+#     print(l1[i])
+
+# l2=[11,22,33,44,55,66,77,88,99]
+# print(list(range(0,len(l2),2)))
+# print(sum(l2))
+# for i in range(len(l2)):
+#     print(l2[i])
+# result=0
+# for i in l2 :
+#     result = result + i 
+# print (result)
+
+
+# tpl = (1,2,3,4)
+# for i in tpl:
+#      print(i)
+
+# result=0
+# for i in tpl :
+#     result = result + i 
+# print (result)
+
+# set ={1,2,3,4,5}
+# for i in set:
+#     print(i)
+
+# keys = ["name","age",]
+# values = ["abhishek",20]
+# mydict = dict((keys,values))
+# print(mydict)
+
+
+
+#                        ---------------------------------project 2-----------------------------------
+
+# student_name = (input("enter student name :"))
+
+# M1 = int(input("enter marks of the first subject:"))
+
+# M2 = int(input("enter marks of the second subject:"))
+
+# M3 = int(input("enter marks of the third subject: "))
+
+# M4 = int(input("enter marks of the fourth subject: "))
+
+# M5 = int(input("enter marks of the fifth subject: "))
+
+# total_marks = (M1 + M2 + M3 + M4 + M5)
+
+# Total_percentage = ((total_marks)/5)
+
+# CGPA1 = (total_marks / 500)*10
+
+
+# print("total marks=",total_marks)
+# print("total percentage",Total_percentage)
+# print("CGPA : ", CGPA1 )
+# if Total_percentage>=90:
+#     print("Passed with grade A")
+# elif Total_percentage>=80 and Total_percentage<=90:
+#     print("Passed with grade B")
+# elif Total_percentage>=70 and Total_percentage<=80:
+#     print("Passed with grade C")
+# elif Total_percentage>=60 and Total_percentage<=70:
+#     print("Passed with grade D")
+# elif Total_percentage>=50 and Total_percentage<=60:
+#     print("Passed with grade E")
+# else :
+#     print("failed")
