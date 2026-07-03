380d9d976cf964c672d3a952f8f63ea53f0fb182
Abhishek Maurya <mauryaabhishek5624@gmail.com>
2026-07-03 10:06:55 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,125 @@
+a = 10 
+print(type(a))
+
+y = 2.5
+print(type(y))
+
+print(1+1)
+
+String = "srmcem"
+print(String)
+print(type(String))
+print(len(String))
+print(String[0])
+print(String[3])
+print(String[0:3])
+print(String[0:6:2])  
+print(String[-1])
+print(String[-6])
+print(String[-6:-1])
+print(String[-6:-1:2])
+
+print(String[-1:-7:-1])
+print (String[0:3:-5])
+
+s = False
+t = True
+print(s*t)
+print(s+t)
+print(s/t)
+print(s-t)
+
+t = "batch name is Computer Science and Engineering"
+print(t)
+print(type(t))
+print(len(t))
+print(t[0])
+print(t[3])
+print(t[0:3])
+print(t[0:6:2])
+print(t[-1])
+print(t[-6])
+print(t[-6:-1])
+print(t[::-1])
+print(t[-1:-44:-5])
+
+l=[1,2,3,4,"abhishek",99.99,3+7j]
+print(l)
+print(type(l))
+print(len(l))
+print(l[6].real)
+print(l[6].imag)
+
+s = "RAMSWAROOP"
+print(s.count('O'))
+print(s.find('P'))
+print(s.find(''))
+print(s.lower())
+print(len(s))
+print(l[::-1])
+print(l[5])
+print(l.append(67))
+print(l.pop(1))
+print(l.pop(2))
+print(l.pop(4))
+print(l.insert(0,55))
+print(l)
+
+
+
+t = (1,2,3,4,"abhishek",99.99,3+7j)
+print(type(t))
+print(len(t))
+print(t.count(4))
+print(t[4].count('h'))
+print(t.lower())   #not possible immuartble
+
+
+dict={'name':'age','abhishek':21,'ankit':21}
+print(dict['name'])
+print(dict)
+
+
+
+st = {1,2,3,4,5}
+print(type(st))
+print(len(st))
+print(st.count(2))
+
+
+marks = 87
+
+if marks >= 90:
+    print("A+")
+elif 80 <= marks < 90:
+    print("A")
+elif 70 <= marks < 80:
+    print("B+")
+elif 60 <= marks < 70:
+    print("B")
+else:
+    print("F")
+
+
+
+menu = {
+    "chhole bhatoore": 120,
+    "pav bhaji": 180
+}
+
+print(menu)
+
+
+item = input("Enter Item Name: ")
+
+
+if item in menu:
+    qty = int(input("Enter Quantity: "))
+    total = menu[item] * qty
+
+    print("Selected Item :", item)
+    print("Total Amount = ₹", total)
+else:
+    print("Sorry! Item not available.")
+
+
